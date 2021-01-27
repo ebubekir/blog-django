4148add501ef1a1b4e1f9c64a38cd5080b71373d
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2021-01-27 12:03:58 +0300

    Weightign search queries

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -90,9 +90,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title','body')
+            search_vector = SearchVector('title',weight='A') + SearchVector('body',weight='B')
             search_query = SearchQuery(query)
-            results = Post.published.annotate(search = search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
+            results = Post.published.annotate(search = search_vector, rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
     return render(request, 'blog/post/search.html', {'form':form,
                                                       'query':query,
                                                       'results':results})
